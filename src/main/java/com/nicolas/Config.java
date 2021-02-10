c0da6b10d374127a2fe26d46c3a927c3c6ab2594
Nicolas Portela <nmpc1997@gmail.com>
2021-02-10 01:42:34 -0300

    alterado para System.getenv o Config.java

--- src/main/java/com/nicolas/Config.java
+++ src/main/java/com/nicolas/Config.java
@@ -1,13 +1,14 @@
 package com.nicolas;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 	
-	private static final Dotenv dotenv = Dotenv.load();
+//	private static final Dotenv dotenv = Dotenv.load();
 	
 	public static String get(String key) {
-		return dotenv.get(key.toUpperCase());
+		return System.getenv(key.toUpperCase());
+//		return dotenv.get(key.toUpperCase());
 	}
 
 }
